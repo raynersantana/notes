3f6f16b4f1a812353bccb87b748c6c1927629a25
Rayner Santana <raynersantana@plusoft.com>
2024-02-07 20:11:14 -0300

    Adding some hooks and styling to Dialogs

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNote } from './components/new-note-card'
 import { Notecard } from './components/note-card'
 
+const note = {
+  date: new Date(),
+  content: 'Hello World'
+}
+
 export function App() {
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -17,9 +22,8 @@ export function App() {
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNote />
-        <Notecard />
-        <Notecard />
-        <Notecard />
+        <Notecard note={note}/>
+        <Notecard note={note}/>
       </div>
     </div>
   )
